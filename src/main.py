b26b295fb68b03468bb002c924c023f7d003f107
Ckrisirkc <gotaques@yahoo.com>
2015-06-01 20:58:15 -0700

    Updated to basic working version

--- src/main.py
+++ src/main.py
@@ -1,50 +1,146 @@
 ### Chris Tesoriero
+
+from toolclasses import *
+
 import os
 import shutil
 import eyed3
+import sys
 
-#from eyed3.id3 import Tag
-#from sys import exit
+SONGS_FOLDER = None
+EXPORT_FOLDER = None
+SETTINGS = None
+SONG_MANAGER = None
+BMFOLDERS = None
 
-
-# returns the Mp3 file name from an .osu file
-def getMp3FileName(file):
-	reader = open(file, 'r')
-	for line in reader:
-		if line.startswith("AudioFilename"):
-			#print("Line: %s"  % line)
-			return line[line.index(":")+1:].strip()
-	return "Not Found"
-
-def donothing():
-	pass
+def main():
+    global EXPORT_FOLDER
+    # Start by finding the songs folder
+    setSongsFolder()
+    # Initial parse of the songs folder
+    parseSongsFolder()
+    if not(os.path.isdir("export")):
+        os.mkdir("export")
+    EXPORT_FOLDER = os.path.abspath("export")
+    mainMenu()
     
-# returns the Mp3 file names from an osu! song folder
-def getMp3Names(directory):
-	names = []
-	for f in os.listdir(directory):
-		#print("dir %s" % f) if os.path.isdir(f) else donothing()
-		if ".osu" in os.path.splitext(f)[1].lower():
-			names.append(getMp3FileName(os.path.join(directory,f)))
-	return list(set(names))
-#print(os.path.join(os.getcwd(), "songs")) if os.path.isdir("songs") else print("no")
-#print(getMp3Names("C:\\Users\\Chris\\Documents\\CS\\151878 Chasers - Lost"))
 
-if not(os.path.isdir("songs")):
-	exit("No songs")
-if not(os.path.isdir("extracted")):
-	os.mkdir("extracted")
 
-#songTags = dict()
+def setSongsFolder(desc=False):
+    global SONGS_FOLDER
+    if desc:
+        rtn = "Set songs folder"
+        if (SONGS_FOLDER == None):
+            rtn += " Currently '" + os.path.abspath(SONGS_FOLDER)
+        return rtn
+    if not(os.path.isdir("songs")):
+        sys.exit("No songs")
+    SONGS_FOLDER = os.path.join(os.getcwd(), "songs")
+    return
 
-songsFolder = os.path.join(os.getcwd(), "songs")
-folderList = [os.path.join(songsFolder, f) for f in os.listdir(songsFolder)]
-for dir in folderList:
-	if not os.path.isdir(dir):
-		continue
-	songPaths = [os.path.join(dir, song) for song in getMp3Names(dir)]
-	for song in songPaths:
-		shutil.copy(song, "extracted")
-        audiofile = eyed3.load(os.path.join("extracted", song))
-        audiofile.tag.album = (u"osu! folder")
-        audiofile.tag.save()
+def parseSongsFolder(desc=False):
+    global SONG_MANAGER
+    global BMFOLDERS
+    if desc:
+        rtn = "Parse the current songs folder"
+        return rtn
+    if (SONGS_FOLDER == None):
+        sys.exit("Songs Folder None")
+    folderList = [os.path.join(SONGS_FOLDER, f) for f in os.listdir(SONGS_FOLDER)]
+    BMFOLDERS = []
+    for dir in folderList:
+        if not os.path.isdir(dir):
+            continue
+        bmf = BMFolder(dir)
+        if bmf.isValid():
+            BMFOLDERS.append(bmf)
+    SONG_MANAGER = SongManager(BMFOLDERS)
+    return
+
+def cprn(file, name):
+    fileName = os.path.basename(file)
+    ext = os.path.splitext(fileName)[1]
+    if (ext == ""):
+        ext = os.path.splitext(".mp3")[0]
+    newFile = os.path.join(EXPORT_FOLDER, fileName)
+    counter = 0
+    while(os.path.isfile(newFile)):
+        newFile = os.path.join(EXPORT_FOLDER, os.path.splitext(fileName)[0] + str(counter) + os.path.splitext(fileName)[1])
+        counter += 1
+    shutil.copy(file, newFile)
+    counter = 0
+    rname = name + ext
+    while (os.path.isfile(os.path.join(EXPORT_FOLDER, rname))):
+        rname = name + str(counter) + ext
+        counter += 1
+    os.rename(newFile, os.path.join(EXPORT_FOLDER, rname))
+    return os.path.join(EXPORT_FOLDER, rname)
+
+def exportSongs(desc=False):
+    if desc:
+        rtn = "Export all currently selected songs"
+        return rtn
+    counter = 1
+    for song in SONG_MANAGER.getSongs():
+        #os.rename(filename, newfilename)
+        name = song.getArtist() + "-" + song.getTitle()
+        name = name.replace(" ", "_")
+        valid_chars = '-_.() abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+        name = ''.join(c for c in name if c in valid_chars)
+        exported = cprn(song.getFileName(), name)
+        try:
+            audiofile = eyed3.load(exported)
+        except UnicodeDecodeError:
+            print "Unicode Error with file %s, cannot properly edit file." % os.path.basename(exported)
+            continue
+        if audiofile is None:
+            print "Could not edit tags for %s, because it is not a supported file type!" % os.path.basename(exported)
+            continue
+        filetag = audiofile.tag
+        if filetag is None:
+            filetag = eyed3.id3.Tag()
+            filetag.file_info = eyed3.id3.FileInfo(exported)
+            filetag.version = (2,3,0)
+        else:
+            filetag.clear()
+            filetag.file_info = eyed3.id3.FileInfo(exported)
+            filetag.version = (2,3,0)
+        filetag.album = (u"osu! folder")
+        filetag.artist = unicode(song.getArtist())
+        filetag.title = unicode(song.getTitle())
+        filetag.track_num = (counter, None)
+        filetag.save()
+        counter += 1
+
+def done(desc=False):
+    if desc:
+        rtn = "Exit"
+        return rtn
+    sys.exit("User Quit")
+def fallthru(desc=False):
+    if desc:
+        rtn = "Back"
+    pass
+
+def menu(choices, header):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    for toprint in header:
+        print toprint
+    for n in range(len(choices)):
+        print "%i) %s" % (n+1, choices[n](True))
+    choice = userChoice([x + 1 for x in range(len(choices))])
+    choices[int(choice)-1]()
+    
+def mainMenu(desc=False):
+    if desc:
+        rtn = "Main Menu"
+        return rtn
+    
+    header = "Using songs folder '%s'\nContaining %i Song Folders, with a total of %i beatmaps for %i songs" % \
+        (SONGS_FOLDER, len(BMFOLDERS), sum([len(bmf.getBeatmaps()) for bmf in BMFOLDERS]), SONG_MANAGER.getNumSongs())
+    choices = [exportSongs, done]
+    while True:
+        menu(choices, [header])
+
+main()
+print "Ended"
--- src/toolclasses.py
+++ src/toolclasses.py
@@ -1,11 +1,5 @@
-#class Test:
-#	def __init__(self, a, b):
-#		self.x = a
-#		self.y = b
-#	def __str__(self):
-#		return "("+str(self.x)+","+str(self.y)+")"
-#	def __add__(self, other):
-#		return Test(self.x + other.x, self.y + other.y)
+import os
+import re
 
 class BMSong:
     def __init__(self, fileName):
@@ -17,9 +11,10 @@ class BMSong:
         return (self.fileName == other.fileName)
     def __ne__(self, other):
         return (self.fileName != other.fileName)
+    def __hash__(self):
+        return hash(self.fileName)
     def __str__(self):
         return ("%s - %s - %s" % (self.artist, self.title, self.fileName))
-        
     def setArtist(self, artist):
         self.artist = artist
     def setTitle(self, title):
@@ -30,27 +25,80 @@ class BMSong:
         return self.artist
     def getTitle(self):
         return self.title
+    def getSize(self):
+        return os.path.getsize(fileName)
+    def search(self, keyword):
+        return (re.search(keyword, self.Title, re.IGNORECASE) or re.search(keyword, self.Artist, re.IGNORECASE))
 
 class BMFolder:
     def __init__(self, folder):
         self.folder = folder
         self.songs = set()
+        self.beatmaps = set()
         for file in os.listdir(folder):
             if ".osu" in os.path.splitext(file)[1].lower():
-                getInfoFromFile(file)
-			
-    def getInfoFromFile(file):
+                filep = os.path.join(folder, file)
+                self.beatmaps.add(filep)
+                self.getInfoFromFile(filep)
+    def getInfoFromFile(self, file):
         reader = open(file, 'r')
         for line in reader:
             if line.startswith("AudioFilename"):
                 #print("Line: %s"  % line)
-                songPath = line[line.index(":")+1:].strip()
-            if line.startswith("Title"):
-                songTitle = line[line.index(":")+1:].strip()
-            if line.startswith("Artist"):
-                songArtist = line[line.index(":")+1:].strip()
+                songPath = os.path.join(self.folder, line[line.index(":")+1:].strip())
+            if line.startswith("Title:"):
+                songTitle = line[line.index(":") + 1:].strip()
+            if line.startswith("Artist:"):
+                songArtist = line[line.index(":") + 1:].strip()
         if songPath is None:
             return
         song = BMSong(songPath)
         if song in self.songs:
             return
+        song.setArtist(songArtist)
+        song.setTitle(songTitle)
+        self.songs.add(song)
+    def __str__(self):
+        return ("%s, %s Beatmaps for %s song" % (self.folder, len([name for name in os.listdir(self.folder) if "osu" in os.path.splitext(name)[1].lower()]), len(self.songs)))
+    def getSongs(self):
+        return self.songs
+    def getBeatmaps(self):
+        return self.beatmaps
+    def isValid(self):
+        return (len(self.songs) > 0)
+
+class SongManager:
+    def __init__(self, bmfolders):
+        self.songs = [individualsong for bmfolder in bmfolders for individualsong in list(bmfolder.getSongs()) if os.path.isfile(individualsong.getFileName())] # This makes my head hurt. Get a list of all songs from all BMFolders in bmfolders
+        self.selected = list(self.songs)
+    def filterSongs(self, keyword):
+        self.selected = [match for song in self.songs if song.search(keyword)]
+        # equivalent code below --
+        #for song in self.songs:
+        #    if song.search(keyword):
+        #        self.selected.add(song)
+    def unfilterSongs(self):
+        self.selected = list(self.songs)
+    def getNumSongs(self, allSongs=False):
+        songlist = self.songs if allSongs else self.selected
+        return len(songlist)
+    def getTotalFileSize(self, allSongs=False):
+        songlist = self.songs if allSongs else self.selected
+        return sum([song.getSize() for song in songlist])
+    def getSongs(self, allSongs=False):
+        songlist = self.songs if allSongs else self.selected
+        return songlist
+
+    
+def userChoice(choices=None):
+    usrin = raw_input("Enter Option: ").strip()
+    if (choices is None):
+        return usrin
+    while (not(usrin.lower() in [str(x).lower() for x in choices])):
+        print "Invalid Option {%s}" % usrin
+        print "Avilable Options: ",
+        for x in choices:
+            print x,
+        usrin = raw_input("Enter Option: ").strip()
+    return usrin
+        
